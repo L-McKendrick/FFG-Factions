3282c7e72a532d88d4f144a12d1de1b3c018deac
Liam McKendrick <liammckendirckgaming@gmail.com>
2024-09-25 13:38:43 -0700

    fix at classname in cfgGroups: sia_arng_infantry_squad

--- addons/sia_arng/cfgGroups.hpp
+++ addons/sia_arng/cfgGroups.hpp
@@ -37,7 +37,7 @@ class CfgGroups {
                         position[] = {10,-10,0};
                         rank = "PRIVATE";
                         side = 1;
-                        vehicle = "sia_arng_rifleman_at";
+                        vehicle = "sia_arng_rifleman_lat";
                     };
                     class Unit4 {
                         position[] = {-10,-10,0};
